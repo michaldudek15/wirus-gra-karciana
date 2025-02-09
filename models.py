a6a541884be319184591ac4cb3cf38b4493f8b8b
Michał Dudek <michalpaweldudek@gmail.com>
2025-02-09 23:00:58 +0100

    dodałem id karty

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
+import uuid
+
 class Card:
     "Bazowa klasa reprezentująca kartę."
     def __init__(self, name: str, description: str):
+        self.id = str(uuid.uuid4())
         self.name = name
         self.description = description
     
