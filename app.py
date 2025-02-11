b87aee30de5883b45a169e5f8f6655e76ad64188
Michał Dudek <michalpaweldudek@gmail.com>
2025-02-11 18:43:42 +0100

    próba zrobienia wymiany kart

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template, url_for, request, redirect, flash, session
+from flask import Flask, render_template, url_for, request, redirect, flash, session, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from deck import buildDeck
@@ -34,7 +34,7 @@ def graSingleplayer():
 
     testowaKarta1 = Organ("serce", "red", "opis")
     testowaKarta2 = Organ("mózg", "blue", "opis")
-    testowaKarta3 = Organ("kość", "green", "opis")
+    testowaKarta3 = Organ("kość", "yellow", "opis")
 
     gra.addPlayer(player1)
     gra.addPlayer(player2)
@@ -52,6 +52,8 @@ def graSingleplayer():
     player2.organsOnTable[testowaKarta3] = testowaKarta3.status
 
     session['gra'] = gra
+    session['player1'] = player1
+    session['player2'] = player2
     return  render_template('gra-singleplayer.html', player1 = player1, player2 = player2)
 
 @app.route('/zacznijGreMultiplayer')
@@ -80,11 +82,11 @@ def zagrajKarte():
     if not gra:
         print ("brak stanu gry w sesji")
 
-    player1 = gra.players[0]
-    player2 = gra.players[1]
+    sourcePlayer = gra.players.get(source_player_id)
+    targetPlayer = gra.players.get(target_player_id)
 
     source_card = None
-    for card in player1.hand:
+    for card in sourcePlayer.hand:
         if card.cardId == source_card_id:
             source_card = card
             break
@@ -93,13 +95,58 @@ def zagrajKarte():
         print("Nie znaleziono karty o id:", source_card_id)
         return "Błąd: Karta nie została znaleziona", 400
 
-    player1.playCard(source_card, gra.deck, gra.discardPile)
-
+    if (sourcePlayer.playCard(source_card, gra.deck, gra.discardPile) == -1):
+        komunikat = "niedozwolony ruch"
+    else:
+        komunikat = ""
 
 
     print(gra)
-    return  render_template('gra-singleplayer.html', player1 = gra.players[0], player2 = gra.players[1])
+    session['gra'] = gra
+    player1 = session.get('player1')
+    player2 = session.get('player2')
+    
+    return  render_template('gra-singleplayer.html', player1 = player1, player2 = player2, komunikat = komunikat)
 
+@app.route('/wymienKarty', methods=['POST'])
+def wymienKarty():
+    source_player_id = request.form.get('source_player_id')
+    first_card_id   = request.form.get('first_card_id')
+    second_card_id = request.form.get('second_card_id')
+    third_card_id   = request.form.get('third_card_id')
+
+    gra = session.get('gra')
+    if not gra:
+        flash("Brak stanu gry w sesji!", "error")
+        return redirect(url_for('graSingleplayer'))
+    
+        # Pobierz gracza, który chce wymienić karty
+    sourcePlayer = gra.players.get(source_player_id)
+    if not sourcePlayer:
+        flash("Nie znaleziono gracza o podanym id!", "error")
+        return redirect(url_for('graSingleplayer'))
+    
+    # Budujemy listę identyfikatorów kart do wymiany
+    card_ids = [first_card_id, second_card_id, third_card_id]
+    cards_to_exchange = []
+    for cid in card_ids:
+        if cid:  # Jeśli pole nie jest puste
+            # Wyszukujemy kartę w ręce gracza o podanym cardId
+            card = next((c for c in sourcePlayer.hand if c.cardId == cid), None)
+            if not card:
+                flash(f"Karta o id {cid} nie znajduje się w ręce gracza!", "error")
+                return redirect(url_for('graSingleplayer'))
+            cards_to_exchange.append(card)
+    
+    # Wykonaj wymianę kart (odrzuć wybrane karty, dobierz nowe)
+    sourcePlayer.discardCardsFromHand(gra.deck, gra.discardPile, *cards_to_exchange)
+    
+    # Zaktualizuj stan gry w sesji
+    print(gra)
+    session['gra'] = gra
+    player1 = session.get('player1')
+    player2 = session.get('player2')    
+    return  render_template('gra-singleplayer.html', player1 = player1, player2 = player2)
 
 # @app.route('/botZagrajKarte', methods=['POST'])
 # def zagrajKarte():
--- models.py
+++ models.py
@@ -70,24 +70,39 @@ class Player:
         self.hand.append(card)
         return card
     
-    def discardCardFromHand(self, card: Card, deck: list, discardPile: list):
-        "Odrzuć kartę na stos kart odrzuconych."
-        discardPile.append(card)
-        self.hand.remove(card)
-        self.drawCard(deck)
+    def discardCardsFromHand(self, deck: list, discardPile: list, *cards):
+        "Odrzuć karty na stos kart odrzuconych."
+        for card in cards:
+            discardPile.append(card)
+            self.hand.remove(card)
+            self.drawCard(deck)
 
     def playCard(self, card: Card, deck: list, discardPile: list, target=None):
         "Zagraj kartę z ręki."
         if card not in self.hand:
             raise ValueError(f"Karta {card.name} nie znajduje się w ręce gracza!")
 
-        self.hand.remove(card)
+        result = 0
 
         if isinstance(card, Organ):
-            if card.name in self.organsOnTable:
-                raise ValueError(f"Organ {card.name} już znajduje się na stole gracza {self.name}!")
-            self.organsOnTable[card.name] = "sterylny"
-            print(f"{self.name} zagrał organ {card.name}.")
+            # Iterujemy po istniejących kluczach w organsOnTable
+            for existing_key in self.organsOnTable.keys():
+                # Jeśli klucz to obiekt posiadający atrybut 'name'
+                if hasattr(existing_key, "name"):
+                    if existing_key.name == card.name:
+                        print("1.Organ {card.name} już znajduje się na stole gracza {self.name}!")
+                        result = -1
+                else:
+                    # Jeśli klucz jest stringiem, porównujemy bezpośrednio
+                    if existing_key == card.name:
+                        print("2.Organ {card.name} już znajduje się na stole gracza {self.name}!")
+                        result = -1
+            # Jeśli nie znaleziono duplikatu, dodajemy obiekt jako klucz
+            if (result == 0):
+                self.hand.remove(card)
+                self.organsOnTable[card] = "sterylny"
+                print(f"{self.name} zagrał organ {card.name}.")
+                self.drawCard(deck)
         
         # TODO
         elif isinstance(card, Szczepionka):
@@ -101,7 +116,7 @@ class Player:
         elif isinstance(card, Terapia):
             print("ZAGRANIE TERAPII\n")
 
-        self.drawCard(deck)
+        return result
 
     def __repr__(self):
         return (f"Player(playerId={self.playerId}, name={self.name}, "
@@ -112,7 +127,7 @@ class GameState:
     "Klasa reprezentująca aktualny stan gry."
     def __init__(self):
         self.gameId = str(uuid.uuid4())
-        self.players = []
+        self.players = {}
         self.currentPlayerId = 0
         self.deck = []  
         self.discardPile = []
@@ -121,14 +136,14 @@ class GameState:
         self.currentPlayerId = (self.currentPlayerId + 1) % len(self.players)
 
     def addPlayer(self, player: Player):
-        self.players.append(player)
+        self.players[player.playerId] = player
 
     def __repr__(self):
         return (
             f"GameState(\n"
-            f"  Aktualny gracz: {self.players[self.currentPlayerId].name if self.players else 'None'},\n"
-            f"  Gracze: {[player.name for player in self.players]},\n"
-            f"  Organy: {[player.organsOnTable for player in self.players]} \n"
+            # f"  Aktualny gracz: {self.players.get[self.currentPlayerId].name if self.players else 'None'},\n"
+            f"  Gracze: {[player.name for player in self.players.values()]},\n"
+            f"  Organy: {[player.organsOnTable for player in self.players.values()]} \n"
             f"  Rozmiar talii: {len(self.deck)},\n"
             f"  Rozmiar stosu kart odrzuconych: {len(self.discardPile)},\n"
             f")"
